0ab2a423ffca3b5c0a129c8742027b971a128a1c
Nhu Tran <mohamat167@yahoo.com.vn>
2026-05-06 12:14:18 +0700

    Update Platform

---
+++ scripts/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "🚀 Create k3d cluster..."
+k3d cluster create gitops \
+  -p "8080:80@loadbalancer" \
+  -p "8443:443@loadbalancer"
+
+echo "📦 Install ArgoCD..."
+kubectl create namespace argocd || true
+
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply --server-side -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "⏳ Waiting ArgoCD..."
+kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=120s
+
+echo "🌐 Port forward ArgoCD..."
+kubectl port-forward svc/argocd-server -n argocd 9090:443 &
+
+echo "✅ Done"
