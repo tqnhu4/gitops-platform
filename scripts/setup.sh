66379bd397b72d6a136ee3aef26e63a1871f4c74
Nhu Tran <mohamat167@yahoo.com.vn>
2026-05-09 14:14:16 +0700

    Update Platform

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "🚀 Create k3d cluster..."
+echo "Create k3d cluster..."
 #k3d cluster create gitops \
 #  -p "8080:80@loadbalancer" \
 #  -p "8443:443@loadbalancer"
@@ -12,16 +12,16 @@ k3d cluster create gitops \
   -p "8443:443@loadbalancer" \
   --k3s-arg "--disable=traefik@server:0"  
 
-echo "📦 Install ArgoCD..."
+echo "Install ArgoCD..."
 kubectl create namespace argocd || true
 
 #kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply --server-side -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-echo "⏳ Waiting ArgoCD..."
+echo "Waiting ArgoCD..."
 kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=120s
 
-echo "🌐 Port forward ArgoCD..."
+echo "Port forward ArgoCD..."
 kubectl port-forward svc/argocd-server -n argocd 9090:443 &
 
-echo "✅ Done"
+echo "Done"
